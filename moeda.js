f61908f916e2a3ddba8533e986b3986516778ad2
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-11-01 11:09:49 -0300

    adicionando o codigo para armaneza a chave da api do site exchangerate

---
+++ moeda.js
@@ -0,0 +1,4 @@
+const apiKey = 'e525988e1697fd6f84538bee'; //armaneza a chave da api do site exchangerate
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`; //valido por um mes
+
+//Função para buscar a taxa de cambio da API
