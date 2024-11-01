994cb2c4893402fb64fe0fac3d8af1ab238ad273
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-11-01 11:29:03 -0300

    adicioando o retorno do valor da moeda ,e mensagem de erro caso esteja a taxa incoreta

--- moeda.js
+++ moeda.js
@@ -4,8 +4,17 @@ const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`; //valido 
 //Função para buscar a taxa de cambio da API
 async function getExchangeRate(moeda1,moeda2) {
     try{
+        const response = await fetch(`${apiURL}${moeda1}`); //Trazendo o link da api 
+        const data = await response.json(); 
+
+        if(data.result === 'success'){
+            return data.conversion_rates[moeda2]; // retorno do valor da moeda
+        }else{
+            throw new Error('Erro ao buscar a taxa de câmbio'); // mensagem de erro caso esteja a taxa incoreta
+            
+        }
 
     }catch(){
-        
+
     }
 }
